8e7ca819d9ba196c2e63382ed800bcb2a22a912a
zxc <zxd21949049@gmail.com>
2021-01-13 13:21:41 +0800

    edit

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,35 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//16384---->screen
+//24576---->keyboard
+@8192               
+D=A               
+@0                
+M=D                
+@24576
+D=M
+@18                  
+D;JNE            
+//WHITE LOOP
+@0
+D=M               
+M=M-1                
+@0              
+D;JEQ           
+@16383          
+A=D+A                
+M=0              
+@8                
+0;JMP           
+//BLACK LOOP
+@0
+D=M
+M=M-1
+@0
+D;JEQ            
+@24576
+A=A-D
+M=-1
+@18
+0;JMP       
